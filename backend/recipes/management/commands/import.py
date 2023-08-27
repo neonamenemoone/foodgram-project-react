d3cd58306c7cfad3aa4b0f2d1f2c54c08be3fe1b
Александр Мокрушин <neonamenemoone@yandex.ru>
2023-08-27 14:20:12 +0300

    import csv

---
+++ backend/recipes/management/commands/import.py
@@ -0,0 +1,21 @@
+import csv
+from django.core.management import BaseCommand
+from recipes.models import Ingredient
+import os
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        # Получаем абсолютный путь к директории с текущим файлом
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        # Формируем путь к файлу ingredients.csv, находящемуся в папке выше
+        csv_file_path = os.path.join(current_directory, '..', '..', '..', '..', 'data', 'ingredients.csv')
+
+        with open(csv_file_path, encoding='UTF-8') as ingredients:
+            reader = csv.reader(ingredients, delimiter=",")
+            for row in reader:
+                Ingredient.objects.get_or_create(
+                    name=row[0], measurement_unit=row[1],
+                )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты загружены успешно'))
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -19,7 +19,7 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     measurement_unit = models.CharField(max_length=50)
 
     def __str__(self):
